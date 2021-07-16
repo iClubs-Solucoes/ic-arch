f7e64aa7d19cf8735d740ccc3578ea03adca380c
Fulvio Coelho <coelho.fulvio@gmail.com>
2021-07-16 18:30:55 -0300

    feat(devenv.sh): added auto install packages for dev in new script

--- installer/devenv.sh
+++ installer/devenv.sh
@@ -9,10 +9,27 @@ packagesDevEnv=(
   "python"
 )
 
+installPakages(){
+  for p in ${packagesDevEnv[@]}; do
+    pacman -S $p --noconfirm
+  done
+}
+
+yayInstalled(){
+  installPakages
+  for aur in ${packagesDevEnvAUR[@]}; do
+    yay -S $aur --noconfirm
+  done
+}
+
+yayNotInstalled(){
+
+}
+
 read -p 'YAY esta instalado?(N,y): ' isYayInstalled
 case $isYayInstalled in
   y)
-
+    yayInstalled
   ;;
   *)
 
