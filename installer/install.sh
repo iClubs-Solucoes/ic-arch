b3376cc9df40a33b22c8154a3f7e9393c98469c0
Fulvio Coelho <coelho.fulvio@gmail.com>
2021-07-18 19:07:03 -0300

    fix(install): fixed efi partition type

--- installer/install.sh
+++ installer/install.sh
@@ -285,7 +285,7 @@ efiDiskSetUP() {
   parted '/dev/'$installDisk mklabel gpt 
   ## Para tornar um disco com label msdos bootable com o grub é necessário deixar libre 2047 sectores antes da primeira partição.
   ## echo i | parted -a opt $installDisk mkpart primary linux-swap 1024 $installDiskSwapSize
-  echo i | parted -a opt '/dev/'$installDisk mkpart primary efi 256 806
+  echo i | parted -a opt '/dev/'$installDisk mkpart primary fat32 256 806
   echo i | parted -a opt '/dev/'$installDisk mkpart primary linux-swap 806 $((806+$installDiskSwapSize))
   echo i | parted -a opt '/dev/'$installDisk mkpart primary ext4 $((806+$installDiskSwapSize)) 100%
   # parted -a opt /dev/$installDisk set 2 boot on
