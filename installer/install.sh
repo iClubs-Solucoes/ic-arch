a3cc84b1643543e68261a9d4357eb75de16c98d4
Fulvio Coelho <coelho.fulvio@gmail.com>
2021-08-02 00:14:22 -0300

    fix(install.sh): fix efiBootInstall

--- installer/install.sh
+++ installer/install.sh
@@ -398,7 +398,8 @@ efiDiskSetUP() {
 
 efiBootInstall() {
   # Instalando GRUB
-  arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+  arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+  # arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
   arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
